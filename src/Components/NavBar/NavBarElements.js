c84d9d200edda15599ad2cd574632107527bb41e
Micah Sterling <msterling89@gmail.com>
2021-12-08 17:39:48 -0600

    add styled NavMenu

--- src/Components/NavBar/NavBarElements.js
+++ src/Components/NavBar/NavBarElements.js
@@ -53,3 +53,14 @@ export const MobileIcon = styled.div`
     cursor: pointer;
   }
 `
+export const NavMenu = styled.ul`
+  display: flex;
+  align-items: center;
+  list-align: none;
+  text-align: center;
+  margin-right: -22px;
+
+  @media screen and (max-width:786) {
+    display: none;
+  }
+`

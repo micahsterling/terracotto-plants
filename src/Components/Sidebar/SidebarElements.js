9ff969588d1de8096367781f4e298ac22a9edc63
Micah Sterling <msterling89@gmail.com>
2021-12-10 17:27:34 -0600

    add style sidebarWrapper/sidebarLink

--- src/Components/Sidebar/SidebarElements.js
+++ src/Components/Sidebar/SidebarElements.js
@@ -28,3 +28,24 @@ export const Icon = styled.div`
   cursor: pointer;
   outline: none;
 `;
+
+export const SidebarWrapper = styled.div`
+  color: #fff;
+`
+
+export const SidebarLink = styled(LinkS)`
+  display: flex;
+  align-item: center;
+  justify-content: center;
+  font-size: 1.5rem;
+  text-decoration: none;
+  list-style: none;
+  transition: 0.2s ease-in-out;
+  color: #fff;
+  cursor: pointer;
+
+  &:hover {
+    color: #01bf71;
+    transition: 0.2s ease-in-out
+  }
+`

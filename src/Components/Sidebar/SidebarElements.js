1fcf7b654159db34b6b55f8b3974acdf87709209
Micah Sterling <msterling89@gmail.com>
2021-12-10 17:36:40 -0600

    add style sideBtnWrappr/sidebarRoute

--- src/Components/Sidebar/SidebarElements.js
+++ src/Components/Sidebar/SidebarElements.js
@@ -1,5 +1,7 @@
 
 import styled from 'styled-components'
+import {link as LinkS} from 'react-scroll'
+import {link as LinkR} from 'react-router-dom'
 import {FaTimes} from 'react-icons/fa'
 
 export const SidebarContainer = styled.aside`
@@ -47,5 +49,30 @@ export const SidebarLink = styled(LinkS)`
   &:hover {
     color: #01bf71;
     transition: 0.2s ease-in-out
+  }
+`
+
+export const SideBtnWrap = styled.div`
+  display: flex;
+  justify-content: center;
+`
+
+export const SidebarRoute = styled(LinkR)`
+  border-radius: 50px;
+  background: #01bf71;
+  white-space: nowrap;
+  padding: 16px 64px;
+  color: #010606;
+  font-size: 16px;
+  outline: none;
+  border: none;
+  cursor: pointer;
+  trasition: all 0.2s ease-in-out;
+  text-decoration: none;
+
+  &:hover {
+    trasition: all 0.2s ease-in-out;
+    background: #fff;
+    color: #010606
   }
 `

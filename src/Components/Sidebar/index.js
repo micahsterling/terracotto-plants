9be6ff9d97a15f6ae9cf9fd3bc251dd1ed8e8876
Micah Sterling <msterling89@gmail.com>
2021-12-09 18:07:22 -0600

    add sidebar imports

--- src/Components/Sidebar/index.js
+++ src/Components/Sidebar/index.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import { CloseIcon } from './SidebarElements'
+import { SidebarContainer, Icon, CloseIcon } from './SidebarElements'
 
 const Sidebar = () => {
   return (
     <SidebarContainer>
-      
+      <Icon>
+        <CloseIcon />
+      </Icon>
     </SidebarContainer>
   )
 }

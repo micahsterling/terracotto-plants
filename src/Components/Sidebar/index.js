4af13a93dc46fafd6c6dda192e5d8721b76189a8
Micah Sterling <msterling89@gmail.com>
2021-12-10 17:16:12 -0600

    add sidebar structure

--- src/Components/Sidebar/index.js
+++ src/Components/Sidebar/index.js
@@ -8,6 +8,23 @@ const Sidebar = () => {
       <Icon>
         <CloseIcon />
       </Icon>
+      <SidebarWrapper>
+
+      <SidebarMenu>
+        <SidebarLink to='about'>
+          About
+        </SidebarLink>
+        <SidebarLink to='discover'>
+          Discover
+          </SidebarLink>
+        <SidebarLink to='services'>
+          Services
+        </SidebarLink>
+      </SidebarMenu>
+      <SideBtnWrap>
+        <SidebarRoute to='/signin'>Sign In</SidebarRoute>
+      </SideBtnWrap>
+      </SidebarWrapper>
     </SidebarContainer>
   )
 }
